a7afc3630de8471bbc468b9a07dbf0fdc0a14ebd
LaurelineAB <laureline.agabibrac@gmail.com>
2023-06-23 12:30:37 +0000

    Etape 3 : Créer la classe Character

--- assets/js/classes/character.js
+++ assets/js/classes/character.js
@@ -1,6 +1,75 @@
 
 class Character {
-
+    #posX;
+    #posY;
+    
+    constructor(posY = 15,posX = 19)
+    {
+        this.#posX = posX;
+        this.#posY = posY;
+    };
+    
+    get posX()
+    {
+        return this.#posX;
+    };
+    set posX (posX)
+    {
+        this.#posX = posX;
+    };
+    
+    get posY()
+    {
+        return this.#posY;
+    };
+    set posX (posY)
+    {
+        this.#posX = posY;
+    };
+    
+    //METHODS
+    moveUp()
+    {
+        if (this.#posY >= 0)
+        {
+            this.posY -=
+            render();
+        }
+    }
+    moveDown()
+    {
+        if (this.#posY <= 19)
+        {
+            this.posY +=
+            render();
+        }
+    }
+    moveLeft()
+    {
+        if (this.#posX >= 0)
+        {
+            this.posX -=
+            render();
+        }
+    }
+    moveRight()
+    {
+        if (this.#posX <= 29)
+        {
+            this.posX +=
+            render();
+        }
+    }
+    render()
+    {
+        let boxes = document.querySelectorAll("section");
+        for (let box of boxes)
+        {
+            box.classList.remove("character");
+            let newPosition = document.querySelector(`.col-${this.#posX}.row-${this.#posY}`);
+            newPosition.classList.add("character");
+        }
+    }
 }
 
 export { Character };
--- assets/js/index.js
+++ assets/js/index.js
@@ -3,4 +3,6 @@ import { Character } from "./classes/character.js";
 
 window.addEventListener("DOMContentLoaded", function(){
     initMap(20, 30);
+    let character = new Character();
+    character.render();
 });
