a394b71d8914a20818eec03d9c9efb516da9eb86
LaurelineAB <laureline.agabibrac@gmail.com>
2023-06-23 12:14:11 +0000

    Etape 2 : Charger les objets sur la carte

--- assets/js/functions/loadObjects.js
+++ assets/js/functions/loadObjects.js
@@ -1,4 +1,19 @@
 export default function loadObjects(objects)
 {
-
+    for (let object of objects)
+    {
+        let box = document.querySelector(`body .col-${object.posX}.row-${object.posY}`);
+        if (object.type === "tree")
+        {
+            box.classList.add("tree");
+        }
+        else if (object.type === "rock")
+        {
+            box.classList.add("rock");
+        }
+        else if (object.type === "stump")
+        {
+            box.classList.add("stump");
+        }
+    }
 }
