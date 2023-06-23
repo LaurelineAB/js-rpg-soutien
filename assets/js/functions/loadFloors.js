ef7131c87767df570c6e2cf0664b27aa609b1d97
LaurelineAB <laureline.agabibrac@gmail.com>
2023-06-23 12:04:45 +0000

    Etape 1 : Charger les textures des sols

--- assets/js/functions/loadFloors.js
+++ assets/js/functions/loadFloors.js
@@ -6,22 +6,21 @@ export default function loadFloors(map)
         for (let j=0; j<map[i].length; j++)
         {
             let floor = map[i][j];
-            // let box = document.querySelector(`section .col-${j} .row-${i}`);
-            // console.log(`.col-${j} .row-${i}`);
             let box = document.getElementById(id);
             
             if (floor === "G")
             {
                 box.classList.add("grass");
-            }if (floor === "W")
+            }
+            else if (floor === "W")
             {
                 box.classList.add("water");
             }
-            if (floor === "S")
+            else if (floor === "S")
             {
                 box.classList.add("sand");
             }
-            if (floor === "D")
+            else if (floor === "D")
             {
                 box.classList.add("dirt");
             }
